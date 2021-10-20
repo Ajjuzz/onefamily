f5e0e9feb261469ab1a40e34fa97ece80d8e6e7f
Ajjuzz <87987726+Ajjuzz@users.noreply.github.com>
2021-10-20 14:35:37 +0530

    Update settings.py

--- onefamily/settings.py
+++ onefamily/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'onefamily.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-   'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 
