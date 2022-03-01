8922a174fdca966039642d38d937f836e0a95b95
Ajjuzz <87987726+Ajjuzz@users.noreply.github.com>
2022-03-01 15:34:28 +0530

    Update settings.py

--- onefamily/settings.py
+++ onefamily/settings.py
@@ -78,15 +78,10 @@ WSGI_APPLICATION = 'onefamily.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#   'default': dj_database_url.config()
-#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.config()
 }
+
 
 
 # Password validation
